1169491e84ed5b04c9a0daa4f48552a7bc6087fc
FlorMezaSaavedra <mezasaavedraflor@gmail.com>
2021-11-17 12:10:38 -0600

    funciones

---
+++ js/funcion.js
@@ -0,0 +1,54 @@
+
+let dinero = prompt("Ingresa la cantidad de dinero en soles que tienes: ");
+
+function verificarCantidad(dinero){
+
+    if (dinero <= 100){
+        alert("Ahorra más, no tienes mucho dinero")
+    } else if(dinero <= 600){
+        alert("Puedes comprarte un celular de marca Motorola")
+    } else if (dinero <= 1000){
+        alert("Puedes comprarte un celular de marca Samsung Galaxy A12")
+    } else if (dinero <= 2000){
+        alert("Puedes comprarte un celular de marca Xiomi mi 11 lite ")
+    }else if (dinero <= 5000){
+        alert("Puedes comprarte un celular de marca iphone ")
+    } else{
+        alert("Mejor cómprate una latop! :D")
+    }
+
+}
+
+verificarCantidad (dinero)
+
+
+
+
+
+
+
+let talla = prompt("Ingresa tu talla en cm: ");
+let peso = prompt("Ingresa tu peso en kg: ");
+
+
+function calcularIMC (size, wigth){
+    IMC = size * wigth;
+    if (IMC) 
+
+    if (dinero <= 100){
+        alert("Ahorra más, no tienes mucho dinero")
+    } else if(dinero <= 600){
+        alert("Puedes comprarte un celular de marca Motorola")
+    } else if (dinero <= 1000){
+        alert("Puedes comprarte un celular de marca Samsung Galaxy A12")
+    } else if (dinero <= 2000){
+        alert("Puedes comprarte un celular de marca Xiomi mi 11 lite ")
+    }else if (dinero <= 5000){
+        alert("Puedes comprarte un celular de marca iphone ")
+    } else{
+        alert("Mejor cómprate una latop! :D")
+    }
+
+}
+
+verificarCantidad (dinero)
